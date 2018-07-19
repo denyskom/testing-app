ede4c5a42fbe517c27f231f808b286484fe3e062
Denys Kompaniiets <denyskom@interlink-ua.com>
2018-07-19 11:23:19 +0300

    add css changes

--- src/Models/Activity.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const MeetupSchema = mongoose.Schema({
-    title: {type: String, required: true},
-    date: {type: Date, required: true},
-    createdAt:{type: Date, default: Date.now}
-});
--- src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app_db');
-
-
-app.listen(3000, function () {
-    console.log("server started, port 3000")
-});
