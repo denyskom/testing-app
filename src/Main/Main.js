69928e3c6a3ca0d452325fabd06a2f955b9143c8
Denys Kompaniiets <denyskom@interlink-ua.com>
2018-08-03 14:58:44 +0300

    secure login and registration routes

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,5 +1,5 @@
 import Head from "./Head"
-import React from 'react';
+import React, {Component} from 'react';
 import './Main.css'
 import {Route} from 'react-router-dom';
 import {Redirect} from 'react-router-dom';
@@ -10,35 +10,43 @@ import Registration from "../Registration/Registration";
 import Login from "../Login/Login";
 import Logout from "../Login/Logout";
 import routes from '../Main/Routes';
+import store from '../store';
 
-const main = () => {
-    return(
-        <div>
-            <Route path="/" exact render={() => <Redirect to={`./${routes.appHomeRelative}`}/>}/>
-            <Route path={`/${routes.appHomeRelative}/`} component={Head}/>
 
-            <Route path={`/${routes.appHomeRelative}`}  render={
-                () =>
-                    <div className="under-header">
-                    <main className="custom-container">
-                        <Route path={`/${routes.appHomeRelative}`} exact component={ActivityPresentationList}/>
-                        <Route path={`/${routes.appPeopleRelative}`} exact component={Person}/>
-                        <Route path={`/${routes.appPeopleRelative}/:id`} exact component={Person}/>
-                        <Route path={`/${routes.appActivitiesRelative}/:id`}  exact component={FullActivity}/>
-                    </main>
-                </div>
-            }/>
-            <Route path={`/${routes.appIndexRelative}`}  render={() =>
-                <div className="blank">
-                    <Route path={`/${routes.appRegistrationRelative}`} exact component={Registration}/>
-                    <Route path={`/${routes.appLoginRelative}`} exact component={Login}/>
-                    <Route path={`/${routes.appLogoutRelative}`} exact component={Logout}/>
-                </div>
-            }/>
-        </div>
 
-    );
-};
+class Main extends Component {
+    render() {
+        return(
+            <div>
+                <Route path="/" exact render={() => <Redirect to={`./${routes.appHomeRelative}`}/>}/>
+                <Route path={`/${routes.appHomeRelative}/`} component={Head}/>
 
-export default main;
+                <Route path={`/${routes.appHomeRelative}`}  render={
+                    () =>
+                        <div className="under-header">
+                            <main className="custom-container">
+                                <Route path={`/${routes.appHomeRelative}`} exact component={ActivityPresentationList}/>
+                                <Route path={`/${routes.appPeopleRelative}`} exact component={Person}/>
+                                <Route path={`/${routes.appPeopleRelative}/:id`} exact component={Person}/>
+                                <Route path={`/${routes.appActivitiesRelative}/:id`}  exact component={FullActivity}/>
+                            </main>
+                        </div>
+                }/>
+                <Route path={`/${routes.appIndexRelative}`}  render={() =>
+                    <div className="blank">
+                        <Route path={`/${routes.appRegistrationRelative}`} exact
+                               render={() => !store.getState().auth.isAuthenticated?<Registration/>:<Redirect to="../../"/>}/>
+                        <Route path={`/${routes.appLoginRelative}`} exact
+                               render={() => !store.getState().auth.isAuthenticated?<Login/>:<Redirect to="../../"/>}/>
+                        <Route path={`/${routes.appLogoutRelative}`} exact component={Logout}/>
+                    </div>
+                }/>
+            </div>
+
+        );
+    }
+}
+
+
+export default Main;
 
